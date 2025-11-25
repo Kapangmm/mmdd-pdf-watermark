8959495e834ec2496fa27b75aeb0354d81fafa2e
Kapangmm <skun.scg@gmail.com>
2025-11-25 10:53:42 +0630

    Update default watermark description parameters

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ const (
     defaultWatermarkText = "MMDD"
 
     // วางกลางหน้า หมุน 45 องศา โปร่ง 25%
-    defaultWatermarkDesc = "pos:cc, op:0.25, rot:45"
+     defaultWatermarkDesc = "pos:c, rot:45, points:48, op:0.6"
 )
 
 
